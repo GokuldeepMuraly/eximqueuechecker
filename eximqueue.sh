bf184952deb2355d294af41a16d02792b10f60c4
Gokuldeep  T M <gokuldeep@dev1.admod.net>
2017-10-22 17:59:07 +0530

    first commit

---
+++ eximqueue.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "SORTED RESULT"
+echo "-------------"
+echo "Dovecot_plain"
+egrep -o 'dovecot_plain[^ ]+' /var/log/exim_mainlog | sort|uniq -c|sort -nk 1 |tail -n 5
+echo "Dovecot_login"
+egrep -o 'dovecot_login[^ ]+' /var/log/exim_mainlog | sort|uniq -c|sort -nk 1 |tail -n 5
+echo "Directory_based"
+grep "cwd=/home" /var/log/exim_mainlog | awk '{for(i=1;i<=10;i++){print $i}}' | sort | uniq -c | grep cwd | sort -n |tail -n 5
+echo "Exim queue"
+exim -bpc
+
